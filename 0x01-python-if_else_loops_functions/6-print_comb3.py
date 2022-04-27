20aaaa156ae6e6ca5af45b892266a67267bfcaf7
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-04-27 03:24:55 +0100

    print numbers with two different digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if (number/10) < (number%10):
+    if (number / 10) < (number % 10):
         if number != 89:
             print(f"{number:02}", end=", ")
         else:
