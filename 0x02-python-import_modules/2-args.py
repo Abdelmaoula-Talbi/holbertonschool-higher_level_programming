d0eb2ce8d0c17877b7787ca0f2b8cb419f3da5f9
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-04-29 12:32:13 +0100

    count and print the sys.argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv) - 1
+    if argc == 0:
+        print(f"{argc} arguments.")
+    elif argc == 1:
+        print(f"{argc} argument:")
+    elif argc > 1:
+        print(f"{argc} arguments:")
+    for i in range(1, argc + 1):
+        print(f"{i}: {str(argv[i])}")
