7413f3505674eb0b9c3d08a6c059e2ecee6dc06b
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-05-03 01:11:52 +0100

    replace element of a list but just in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     my_new_list = my_list.copy()
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    elif idx >= len(my_list):
-        return my_list
-    my_new_list.pop(idx)
-    my_new_list.insert(idx, element)
+    else:
+        my_new_list.pop(idx)
+        my_new_list.insert(idx, element)
     return my_new_list
