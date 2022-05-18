67a9e54b88f41e3e801aae3fff7ddb211110261d
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-05-18 01:17:36 +0100

    create a class with private instance attribute and a public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Represents a class that defines a square
+by private attribute size
+and calculate the square area.
+
+"""
+
+
+class Square:
+    """Initializes the class
+
+    Args:
+        size (int): The paramater to define the square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size * self.__size 
