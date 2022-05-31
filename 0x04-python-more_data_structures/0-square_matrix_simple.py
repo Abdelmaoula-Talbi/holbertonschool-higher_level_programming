43fac7ba28e53a289f512cfda5e7af653147b235
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-05-31 17:24:32 +0100

    fix pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,8 +4,10 @@ value of all integers of a matrix.
 """
 
 new_matrix = []
+
+
 def square_matrix_simple(matrix=[]):
     for i in matrix:
-        l = list(map((lambda x: x ** 2), i))
-        new_matrix.append(l)
+        new_element = list(map((lambda x: x ** 2), i))
+        new_matrix.append(new_element)
     return new_matrix
