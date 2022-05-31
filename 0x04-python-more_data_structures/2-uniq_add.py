ad6c21ef7e82b8544bc7e7d88d2e73873b58ba7e
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-05-31 18:49:49 +0100

    function that adds all unique integers in a list (only once for each integer).

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uniq_add(mylist=[]):
+    """function that adds all unique integers in a
+    list (only once for each integer)."""
+
+    new_list = set(mylist)
+    sum = 0
+    for i in new_list:
+        sum += i
+    return sum
+    
