7dc4aa1d9c48a40b16904fb9e189f267e4c66ab2
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-06-04 16:13:11 +0100

    fix code for empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@
 def best_score(a_dictionary):
     """ function that returns a key
     with the biggest integer value"""
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     biggest_value = max(a_dictionary, key=lambda x: a_dictionary[x])
     return biggest_value
