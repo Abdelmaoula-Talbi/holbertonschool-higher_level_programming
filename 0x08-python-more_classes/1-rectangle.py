66a4e511c4a63bb97937b6aecabd50d456d2d82c
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-06-11 21:06:51 +0100

    fix code for attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,16 +5,8 @@
 class Rectangle:
     """A class that defines a rectangle"""
     def __init__(self, width=0, height=0):
-#        if type(width) is not int:
- #           raise TypeError("width must be an integer")
-  #      if width < 0:
-   #         raise ValueError("width must be >= 0")
-    #    if type (heigth) is not int:
-     #      raise TypeError("height must be an integer")
-      #  if height < 0:
-       #     raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
