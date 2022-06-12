da9f637d5759a3de735068f2ae9fb6aa24e2e8d8
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-06-12 21:06:48 +0100

    fix code for cases 9 and 10

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -71,5 +71,4 @@ class Rectangle:
             raise TypeError("rect2 must be an instance of Rectangle")
         if rect1.area() >= rect2.area():
             return rect1
-        else:
-            return rect2
+        return rect2
