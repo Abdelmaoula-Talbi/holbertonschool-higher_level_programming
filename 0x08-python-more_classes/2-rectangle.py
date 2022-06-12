8746c140cebbf25830a4ffda86ca9471be79377b
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-06-12 16:33:05 +0100

    create a class Rectangle that defines a rectangle with two private instance attributes and two public instance methods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -34,6 +34,6 @@ class Rectangle:
     def area(self):
         return self.__width * self.__height
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.width + self.height) * 2
+        return (self.__width + self.__height) * 2
