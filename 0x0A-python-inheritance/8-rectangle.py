e5227d1e5ade04fb206337b62b58024db2fa59d3
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-07-04 13:30:16 +0100

    fix pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Represents a class that inherits from another class"""
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """A class Rectangle that inherits from class BaseGeometry"""
     def __init__(self, width, height):
