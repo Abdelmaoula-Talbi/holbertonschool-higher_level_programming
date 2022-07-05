70798358525300a0613638e6ccd509db087a9ef1
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-07-05 15:36:00 +0100

    class Square that inherits from class Rectangle and print new string with function str

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Represents a class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class Square that inherits from class Rectangle"""
+
+    def __init__(self, size):
+        """initiation"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Represents the area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
