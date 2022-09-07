9b370df0c34f927e3b1ef652f4e9e4d916235a9f
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-07 20:02:35 +0100

    fix code

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    for i in range(98):
-        a = i ** a
-    for j in range(98):
-        b = j + b
-    return(a, b)
+    return((a + b) ** 98)
