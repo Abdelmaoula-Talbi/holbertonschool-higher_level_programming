9cf7ef84e0a764391eaefaf84fb51e07de706fa7
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-08 20:37:42 +0100

    prints all possible different and smallest combinations of two digitsx

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if ((i // 10) < (i % 10)):
+        print("{}{}".format((i // 10), (i % 10)), end="")
+        break
+for j in range((i + 1), 100):
+    if ((j // 10) < (j % 10)):
+        print(", ", end="")
+        print("{}{}".format((j // 10), (j % 10)), end="")
+print()
