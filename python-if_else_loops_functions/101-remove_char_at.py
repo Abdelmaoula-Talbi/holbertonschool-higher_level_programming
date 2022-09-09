98cf388e616662b01a02ef6c1854b6e4eb2ddaac
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-09 18:47:12 +0100

    fix code for negative index

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return (str[:n] + str[n+1:])
+    for i in range(len(str)):
+        if (i != n):
+            print("{}".format(str[i]), end="")
+    return ("")
