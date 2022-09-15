22e2cfb3acce195a008103e08cb4e72de7587f55
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-15 13:01:52 +0100

    fix code for negative numbers

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_int = 0
+    if len(my_list) == 0:
+        return None
+    if my_list[0] > my_list[1]:
+        max_int = my_list[0]
+    else:
+        max_int = my_list[1]
     for i in my_list:
         if max_int < i:
             max_int = i
