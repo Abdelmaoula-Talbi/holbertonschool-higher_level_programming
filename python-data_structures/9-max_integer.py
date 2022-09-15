1389a5b2c1702c9800562a54836458911b581a6b
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-15 13:04:20 +0100

    fix code for list with only one integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
+    if len(my_list) == 1:
+        return (my_list[0])
     if my_list[0] > my_list[1]:
         max_int = my_list[0]
     else:
