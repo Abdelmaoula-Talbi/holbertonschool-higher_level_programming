c4ff15b732c422585c32d9082e3d915b29ca8696
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-15 20:22:31 +0100

    fix code for the length of the matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         new_list = []    
-        for i in range(3):
+        for i in range(len(matrix)):
             new_list.append(row[i] ** 2)
         new_matrix.append(new_list)
     return new_matrix
