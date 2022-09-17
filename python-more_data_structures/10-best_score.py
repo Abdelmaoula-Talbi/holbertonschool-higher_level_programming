bf356a44b0e5443384542d4d228155673f13c01c
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-17 14:23:05 +0100

    fix code for the empty dictionaries

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) < 1:
         return None
     sorted_dict = sorted(a_dictionary.items(), key=lambda item: item[1])
     length = len(sorted_dict)
