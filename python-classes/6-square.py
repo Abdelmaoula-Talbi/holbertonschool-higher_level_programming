c7b7633557754c7468df74fe0667546502a6cafc
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-22 18:07:05 +0100

    fix code in the init method for the type of elements of position attribute

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,8 +13,12 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        if (type(position) is not tuple or len(position) != 2
-                or position[0] < 0 or position[1] < 0):
+        if (type(position) is not tuple
+                or len(position) != 2
+                or position[0] < 0
+                or position[1] < 0
+                or type(position[0]) is not int
+                or type(position[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
@@ -41,8 +45,12 @@ class Square:
     @position.setter
     def position(self, value):
         """A setter to set the value for the attribute position"""
-        if (type(value) is not tuple or len(value) != 2
-                or value[0] < 0 or value[1] < 0):
+        if (type(value) is not tuple
+                or len(value) != 2
+                or value[0] < 0
+                or value[1] < 0
+                or type(value[0]) is not int
+                or type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
