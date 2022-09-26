a7365fa0a1ed26d779fbb278acc36780a371aa0e
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-26 11:26:41 +0100

    change code for the line continuation

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,11 +15,17 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    new_list = []
     for row in matrix:
+        new_row = []
         for elem in row:
             if (type(matrix) is not list or type(row) is not list or
                     type(elem) not in (int, float)):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-            if len(row) != len(matrix[0]):
-                raise TypeError("Each row of the matrix must have the same size")
-    return [[round(elem/div, 2) for elem in row] for row in matrix]
+                raise TypeError('matrix must be a matrix (list of lists) \
+of integers/floats')
+            if (len(row) != len(matrix[0])):
+                raise TypeError("Each row of the matrix must \
+have the same size")
+            new_row.append(round(elem/div, 2))
+        new_list.append(new_row)
+    return new_list
