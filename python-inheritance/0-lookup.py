42297c65587f973c638c0f5f525dd3072971f002
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-29 15:33:56 +0100

    add the documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/python3*
+"""A module that representes a function that returns a list of attributes of a given object"""
 def lookup(obj):
+    """A funciton that returns a list of attributes and methods of an object"""
     return dir(obj)
