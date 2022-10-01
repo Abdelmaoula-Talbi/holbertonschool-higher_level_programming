90ece79b37e32d6baeeadf5677c42743af0595b3
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-10-01 16:35:04 +0100

    use the function hasattr to ckeck if the attribute '__dict__' exist in the dir of the object

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
 
-A modulte that representes a function
+A module that representes a function
+that adds attribute to an object
 
 """
 
 
 def add_attribute(My_class, My_att, My_value):
     """
-    A function that adds an attribute to a class
+    A function that adds an attribute to an object
+    if it is possible, otherwise it raises an exception
     """
-    if dir(My_class)[0] != "__class__":
+    if hasattr(My_class, "__dict__") is False:
         raise TypeError("can't add new attribute")
     else:
         setattr(My_class, My_att, My_value)
