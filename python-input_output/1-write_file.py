729dfa659d68c518fac92c8e0cefa8cee2be598a
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-10-04 17:11:23 +0100

    add the documentation for the function write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,5 +8,9 @@ that writes a string to a text file
 
 
 def write_file(filename="", text=""):
+    """
+    A function that writes a string to a text file (utf-8)
+    and returns the number of characters written
+    """
     with open(filename, mode="w", encoding="utf-8") as my_file:
         return my_file.write(text)
