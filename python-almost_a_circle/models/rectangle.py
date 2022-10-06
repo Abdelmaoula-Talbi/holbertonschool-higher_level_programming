33762e5395ffc3dc2faeed3098d530639c1e9a07
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-10-06 18:53:50 +0100

    add the documentation for the methods inside the class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,6 +11,9 @@ class Rectangle(Base):
     A class Rectangle
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        A class constructor to initialise an instance
+        """
         self.__width = width
         self.__height = height
         self.__x = x
@@ -19,32 +22,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter method for the attribute width"""
         return self.__width
 
     @width.setter
-    def width(self, width)
+    def width(self, width):
+        """setter method for the attribute width"""
         self.__width = width
 
     @property
     def height(self):
+        """getter method for the height attribute"""
         return self.__height
 
     @height.setter
     def height(self, height):
+        """setter method for the height attribute"""
         self.__height = height
 
     @property
     def x(self):
+        """getter method for the x attribute"""
         return self.__x
 
     @x.setter
     def x(self, x):
+        """setter method for the x attribute"""
         self.__x = x
 
     @property
     def y(self):
+        """getter method for the y attribute"""
         return self.__y
 
     @y.setter
     def y(self, y):
+        """setter method for the y attribute"""
         self.__y = y
