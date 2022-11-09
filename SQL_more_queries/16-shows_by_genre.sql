2608277d04d96fb7ecbd5bf02db789629a0ba722
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-11-09 16:46:20 +0100

    typo in the syntax

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows LEFT JOIN tv_show_genres, tv_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shos_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.genre_id = tv_genres.id
 ORDER by tv_shows.title, tv_genres.name
