d0b254d64deffd95c5979a3ac9e9920233a8357c
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-11-28 17:35:11 +0100

    add the documentation to the module

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+
+A module that representes a script that lists all states from
+the database hbtn_0e_0_usa
+
+"""
+
 if __name__ == "__main__":
     import MySQLdb
-    db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa")
+    db = MySQLdb.connect(host="localhost", port=3306, user="My_User", passwd="My_Passwd", db="hbtn_0e_0_usa")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
