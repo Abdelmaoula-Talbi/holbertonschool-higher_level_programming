1d06f7e94a1de5c77c421184671abd4ce7a93fde
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-11-28 17:50:05 +0100

    fix code to use the argv

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,8 @@ the database hbtn_0e_0_usa
 
 if __name__ == "__main__":
     import MySQLdb
-    db = MySQLdb.connect(host="localhost", port=3306, user="My_User", passwd="My_Passwd", db="hbtn_0e_0_usa")
+    from sys import argv
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db="hbtn_0e_0_usa")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
