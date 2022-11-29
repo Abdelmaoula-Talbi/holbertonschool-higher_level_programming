7e64fd484e847f237412c30f68aea44a90b31d38
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-11-29 15:45:42 +0100

    fix code for lowercase

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE ASCII(left(name, 1)) == ASCII('N') ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' COLLATE latin1_bin ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
