f4a0460c3757ee1516352ea7f0e06f80095bfbd4
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-05 14:50:20 +0100

    fix code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,13 +9,14 @@ values in the states table of hbtn_0e_0_usa where name matches the argument
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
+    name = argv[4]
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='%({})s' ORDER BY id ASC", (name,).format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name=%(name)s ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
