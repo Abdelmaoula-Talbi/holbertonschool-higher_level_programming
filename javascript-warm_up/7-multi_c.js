29f1b5d14f7e655ab7e75205311d1435367f3970
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-16 17:20:58 +0100

    a script that prints x times a specific string

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let i = 0;
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurences');
+} else {
+  while (i < process.argv[2]) {
+    console.log('C is fun');
+    i++;
+  }
+}
