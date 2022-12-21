b6d6016b1201cb78fb69d65c2c24f40e303c67b1
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-21 17:10:27 +0100

    fix code

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.name FROM cities JOIN states ON states.id = cities.state_id WHERE states.name = %(State_Name)s ORDER BY cities.id", {'State_Name': State_Name})
     rows = cur.fetchall()
     for row in rows:
-        print(" ,".join(row))
+        print(" ,".join(row), end="")
     cur.close()
     db.close()
