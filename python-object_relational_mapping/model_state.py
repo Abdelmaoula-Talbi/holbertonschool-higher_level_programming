938b62ccf077e74fb2534d5007019e937188fd2d
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-12-23 16:47:10 +0100

    add class documentation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 class State(Base):
+    """A class that representes State class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
