109fa0cb8476be0b0c35a6b909b6dfa28cccb129
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2023-01-19 19:39:32 +0100

    fix code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log('code: ' + response.statusCode);
+  }
   console.log('code: ' + response.statusCode);
 });
